3c305e81169d18592ef5afaf481ce8029e438157
Th35kyb0y <mishradeepak073@gmail.com>
2022-02-15 20:24:57 -0800

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 
 export default function App() {
+  //array to filter
+  let names = ['deepak', 'amisha', 'abu bakr', 'piyush', 'ashneer', 'rahul'];
+  // filter method ,returns array that passes a condition in function
+  // we have to store filter method in a var
+  let filtered = names.filter((name) => {
+    return name.indexOf('d') ==!-1;
+    // ddont return the value thhat dodnt exist
+  });
+  console.log(filtered);
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
